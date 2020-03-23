cd299f2991e39b8a09c19b37a65c51b2c9b9d349
Danny Liehr <dannyisyelling@gmail.com>
2020-03-23 11:35:34 -0400

    Added walking sprites. The player now walks on the screen.

--- assets/maps/pokemon_tile.tsx
+++ assets/maps/pokemon_tile.tsx
@@ -1,4 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.2" name="pokemon_tile" tilewidth="16" tileheight="16" tilecount="800" columns="40">
  <image source="../images/pokemon_tile.png" width="640" height="320"/>
+ <tile id="82">
+  <animation>
+   <frame tileid="82" duration="500"/>
+   <frame tileid="83" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="83">
+  <animation>
+   <frame tileid="83" duration="500"/>
+   <frame tileid="82" duration="500"/>
+  </animation>
+ </tile>
 </tileset>

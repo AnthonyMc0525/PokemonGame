b9bed282707fdf9f5e2c0794ded90e27fc7674d4
Anthony McMellen <anthonymc456@gmail.com>
2020-03-25 07:54:42 -0400

    Adds targets for num of targets
    - makes templating easier

--- src/moves/moveTemplate.py
+++ src/moves/moveTemplate.py
@@ -1,10 +1,11 @@
 def MoveTemplate:
-    def __init__(self, name, moveType, pwr, acc, pp, description):
+    def __init__(self, name, moveType, pwr, acc, pp, targets description):
         self.name = name
         self.type = moveType
         self.pwr = pwr
         self.acc = acc
         self.pp = pp
         self.description = description
+        self.targets = targets
 
     #moves from this template will have to create a use method

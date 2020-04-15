ca3ecdc511e651b104a415b00b99ae7e320cb2de
Anthony McMellen <anthonymc456@gmail.com>
2020-04-15 08:51:22 -0400

    Removes unneeded types

--- src/moves/grass/grassTemplate.py
+++ src/moves/grass/grassTemplate.py
@@ -1,18 +1,18 @@
 import moves.moveTemplate
 
 def GrassTemplate(MoveTemplate):
-    def __init__(self, name, moveType="grass", pwr, acc, pp, targets, description)
-    super().__init__(name, moveType, pwr, acc, pp, targets,  description)
+    def __init__(self, name, moveType="grass", pwr, acc, pp, targets, description):
+        super().__init__(name, moveType, pwr, acc, pp, targets,  description)
 
     def use(self, user, target):
-        if user.type = "grass":
+        if user.type == "grass":
             stab = 1.5
         else:
             stab = 1
 
         if target.type == "ground" or target.type == "rock" or target.type == "water":
             eff = 2.0
-        else if target.type == "fire" or target.type == "ice" or target.type == "poison" or target.type == "flying" or target.type == "bug":
+        elif target.type == "fire" or target.type == "ice" or target.type == "poison" or target.type == "flying" or target.type == "bug":
             eff = .5
         else:
             eff = 1

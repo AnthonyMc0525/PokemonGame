cd51eaf074764196bc9c4710dc235a8178b73737
Anthony McMellen <anthonymc456@gmail.com>
2020-04-15 09:00:42 -0400

    Corrects error

--- src/moves/fire/ember.py
+++ src/moves/fire/ember.py
@@ -2,7 +2,7 @@ from . import fireTemplate
 
 def Ember(FireTemplate):
     def __init__(self, name="ember", moveType="attack", pwr=40, acc=1, pp=25, targets=1, description="The target is attacked with small flames. This may also leave the target with a burn."):
-    super().__init__(name, moveType, pwr, acc, pp, targets, description)
+        super().__init__(name, moveType, pwr, acc, pp, targets, description)
 
     def use(self, user, target):
         super().use(user, target)
--- src/moves/fire/flareBlitz.py
+++ src/moves/fire/flareBlitz.py
@@ -2,7 +2,7 @@ from . import fireTemplate
 
 def FlareBlitz(FireTemplate):
     def __init__(self, name="flare blitz", moveType="attack", pwr=120, acc=1, pp=15, targets=1, description="The user cloaks itself in fire and charges the target. This also damages the user quite a lot. This attack may leave the target with a burn."):
-    super().__init__(name, moveType, pwr, acc, pp, targets, description)
+        super().__init__(name, moveType, pwr, acc, pp, targets, description)
 
     def use(self, user, target):
         super().use(user, target)

26593551e3165f18e881dd991f8b3e7c53ada668
Anthony McMellen <anthonymc456@gmail.com>
2020-04-15 08:27:35 -0400

    Fixes __init__

--- src/pokemon/swampert.py
+++ src/pokemon/swampert.py
@@ -2,4 +2,4 @@ from . import pokemonTemplate
 
 class Swampert(PokemonTemplate):
     def __init__(self, name="Swampert", stats={'hp': 100, 'Attack': 110, 'Defense': 90, 'Sp. Atk': 85, 'Sp. Def': 90, 'Speed': 60}, currentHP=310, moves, ailments=[], sprite)
-    super().__init__(name, stats, currentHp, moves, ailments, sprite)
+        super().__init__(name, stats, currentHp, moves, ailments, sprite)
